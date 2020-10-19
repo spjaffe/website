b79f355f05ead2476191ee014430ba82267b0a8c
spjaffe <spj@uchicago.edu>
2020-10-19 16:50:32 -0700

    Add hideshow.js

---
+++ HideShow.js
@@ -0,0 +1,13 @@
+<!--
+
+function expandIt(whichAb,whichMore) {
+	if (whichAb.style.display=='none') {
+		whichAb.style.display = (whichAb.style.display == "none" ) ? "" : "none";
+		whichMore.innerHTML="&#9662 Abstract";	}
+	else {
+		whichAb.style.display ='none';
+		whichMore.innerHTML="&#9656 Abstract";
+	}
+}
+
+//-->
