d3e5ff50b0ffd7e19282abd60761b40ace860a66
spjaffe <spj@uchicago.edu>
2023-12-18 17:11:07 -0800

    fixing PapersMore

--- HideShow.js
+++ HideShow.js
@@ -1,12 +1,24 @@
-<!--
+< !--
 
-function expandIt(whichAb,whichMore) {
+	function expandIt(whichAb, whichMore) {
+		if (whichAb.style.display == 'none') {
+			whichAb.style.display = (whichAb.style.display == "none") ? "" : "none";
+			whichMore.innerHTML = "&#9662 Abstract";
+		}
+		else {
+			whichAb.style.display = 'none';
+			whichMore.innerHTML = "&#9656 Abstract";
+		}
+	}
+
+
+function expandIt2(whichAb,whichMore) {
 	if (whichAb.style.display=='none') {
 		whichAb.style.display = (whichAb.style.display == "none" ) ? "" : "none";
-		whichMore.innerHTML="&#9662 Abstract";	}
+		whichMore.innerHTML="&#9662 Less";	}
 	else {
 		whichAb.style.display ='none';
-		whichMore.innerHTML="&#9656 Abstract";
+		whichMore.innerHTML="&#9656 More";
 	}
 }
 
